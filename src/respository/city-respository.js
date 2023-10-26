4c1e08a3778fa5fa0959c5a15b0a002076c9d354
jatin <jatinsharma2231@gmail.com>
2023-10-26 17:58:49 +0530

    Completed city repository

--- src/respository/city-respository.js
+++ src/respository/city-respository.js
@@ -7,6 +7,7 @@ class CityRepository {
             const city = await City.create({name});
             return city;
         }catch(error){  
+            console.log('Something went wrong in the repository layer');
             throw {error}
         }
     }
@@ -19,7 +20,33 @@ class CityRepository {
                 }
             })
         }catch(error){
+            onsole.log('Something went wrong in the repository layer');
             throw {error}
+        }
+    }
+
+    async updateCity(cityId,data) {
+        try{
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        }catch(error){
+            console.log('Something went wrong in the repository layer')
+            throw {error}
+        }
+    }
+
+    async getCity(cityId) {
+        try{
+            const city = await City.findByPk(cityId)
+            return city;
+        }
+        catch(error){
+            console.log('Something went wrong in the repository layer');
+            throw {error};
         }
     }
 }
